4cd740377fdf058b0418ea48312d879449ceef88
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-29 10:47:00 -0700

    Add integration testing for store error message displays.

--- spec/shoe_store_integration_spec.rb
+++ spec/shoe_store_integration_spec.rb
@@ -18,11 +18,26 @@ describe 'the store CRUD path', {type: :feature} do
     expect(page).to have_content('Crossroads')
   end
 
-  it "allows the user to delet stores" do
+  it "allows the user to delete stores" do
     visit('/stores')
     fill_in('name', with: 'Alderwood')
     click_button('Create Store')
     click_link('Edit')
     click_button('Delete')
+  end
+
+  it "does not allow blank store names" do
+    visit('/stores')
+    fill_in('name', with: '')
+    click_button('Create Store')
+    expect(page).to have_content("Name can't be blank")
+  end
+
+  it "does not allow duplicate store names" do
+    Store.create({name: 'Alderwood'})
+    visit('/stores')
+    fill_in('name', with: 'Alderwood')
+    click_button('Create Store')
+    expect(page).to have_content('Name has already been taken')
   end
 end
