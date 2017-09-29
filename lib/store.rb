0992255e8decd5b4c1664f725efb4d39ce0df92f
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-29 08:59:17 -0700

    Add capitalization callback to store.

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,10 @@
 
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
+  before_save :capitalize_name
+
+private
+  def capitalize_name
+    self.name = self.name.capitalize.split.reduce { |full_name, word| full_name + " " + word.capitalize }
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -3,6 +3,14 @@
 require 'spec_helper'
 
 describe('Store') do
+  describe '#save' do
+    it "capitalizes store name before saving" do
+      store = Store.new({name: 'northGate'})
+      store.save
+      expect(store.name).to eq('Northgate')
+    end
+  end
+  
   describe '#brands' do
     it "returns all brands that a store carries" do
       store = Store.create({name: 'Westlake'})
